22bb73c1f776729459f85d9564cef164cb48309f
Your Name <matej.krchniak@inmail.sk>
2010-02-13 07:00:28 -0800

    opatovne funkcna stara verzia

---
+++ vendor/plugins/graphs/lib/target_version_graph_hook.rb
@@ -0,0 +1,11 @@
+# Provides a graph on the target version page
+class TargetVersionGraphHook < Redmine::Hook::ViewListener
+  def view_versions_show_bottom(context = { })
+  	if !context[:version].fixed_issues.empty?
+  		output = "<fieldset id='target_version_graph'><legend>#{ l(:label_graphs_total_vs_closed_issues) }</legend>"
+		output << tag("embed", :width => "100%", :height => 300, :type => "image/svg+xml", :src => url_for(:controller => 'graphs', :action => 'target_version_graph', :id => context[:version]))
+		output << "</fieldset>"
+		return output
+	end 
+  end
+end
