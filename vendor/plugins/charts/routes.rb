fbad61de43755787483af896c401c6361a4cbcee
Your Name <matej.krchniak@inmail.sk>
2010-02-13 06:19:57 -0800

    odstraneny charts plugin kazil graphs plugin

--- vendor/plugins/charts/routes.rb
+++
@@ -1,7 +0,0 @@
-require_dependency 'redmine_charts/utils'
-
-# Configuring routing for plugin's controllers.
-
-RedmineCharts::Utils.controllers_for_routing do |name, controller|
-  connect "projects/:project_id/charts/#{name}/:action", :controller => controller
-end
