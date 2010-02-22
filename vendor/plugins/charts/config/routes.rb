083ba1cf6c9f44ad8fc9817646edbcd97b730a43
Your Name <matej.krchniak@inmail.sk>
2010-02-22 10:18:16 -0800

    zabudol som switch aby odstranil stare ...

--- vendor/plugins/charts/config/routes.rb
+++
@@ -1,9 +0,0 @@
-require_dependency 'redmine_charts/utils'
-
-# Configuring routing for plugin's controllers.
-
-ActionController::Routing::Routes.draw do |map|
-	RedmineCharts::Utils.controllers_for_routing do |name, controller|
-		map.connect "projects/:project_id/charts/#{name}/:action", :controller => controller
-	end
-end
